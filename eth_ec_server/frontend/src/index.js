3c87edfa7016e9e60caa1a1a6a4a1cba3a26e5cb
masary2989 <fxxkinspam@gmail.com>
2018-12-25 19:28:54 +0900

    can view on the blowser

--- eth_ec_server/frontend/src/index.js
+++ eth_ec_server/frontend/src/index.js
@@ -1,1 +1,9 @@
 import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+//import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+// registerServiceWorker();
